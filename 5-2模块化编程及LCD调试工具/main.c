e04bbf15d8d6dd7fb70794255fa24a9252d421cc
darin <1570906137@qq.com>
2023-05-27 09:06:19 +0800

    C51

--- 5-2模块化编程及LCD调试工具/main.c
+++ 5-2模块化编程及LCD调试工具/main.c
@@ -1,12 +1,15 @@
 #include <REGX52.H>
 #include "LCD1602.h"
-
+#include "Delay.h"
+int result=0;
 void main()
 {
     LCD_Init();
-    LCD_ShowChar(1,1,'A');
-    LCD_ShowString(1,3,"ERROR");
+    
     while(1)
     {
+        LCD_ShowNum(1,1,result,3);
+        result++;
+		Delay(1000);
     }
 }
